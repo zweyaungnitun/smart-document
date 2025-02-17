a4274e1ee61e40881ff1b30a4dea052221d8a63b
zweyaungnitun <zweyaungnitun2020@gmail.com>
2025-02-17 23:25:14 +0630

    controller and services

--- documentmanagement_documentmanagement/src/main/java/com/smartai/documentmanagement/repository/DocumentVersionRepository.java
+++ documentmanagement_documentmanagement/src/main/java/com/smartai/documentmanagement/repository/DocumentVersionRepository.java
@@ -1,6 +1,21 @@
 package com.smartai.documentmanagement.repository;
 
-public interface DocumentVersionRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smartai.documentmanagement.entity.DocumentVersion;
+
+public interface DocumentVersionRepository extends JpaRepository<DocumentVersion, Long>{
+
+//	DocumentVersion save(DocumentVersion version);
+//
+//	Optional<DocumentVersion> findById(Long id);
+//
+	List<DocumentVersion> findByDocumentId(Long documentId);
+//
+//	void deleteById(Long id);
 
 	
 }
